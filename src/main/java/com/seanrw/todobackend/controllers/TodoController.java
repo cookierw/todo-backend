55aa5c84d4a4d6d166c9fe6dc20fbf461a3e7f91
sean-rw <seanrwhitley@gmail.com>
2023-01-06 20:05:27 -0500

    TodoController

---
+++ src/main/java/com/seanrw/todobackend/controllers/TodoController.java
@@ -0,0 +1,53 @@
+package com.seanrw.todobackend.controllers;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.seanrw.models.Todo;
+import com.seanrw.todobackend.services.TodoService;
+
+@RestController
+@RequestMapping("/todos")
+public class TodoController {
+
+    @Autowired
+    private TodoService todoService;
+    
+    @GetMapping("/")
+    public List<Todo> getTodos() {
+        return todoService.getTodos();
+    }
+
+    @GetMapping("/{id}")
+    public Todo getTodoById(long id) {
+        return todoService.getTodoById(id);
+    }
+
+    @PostMapping("/")
+    public Todo addTodo(@RequestBody Todo todo) {
+        return todoService.addOrUpdate(todo);
+    }
+
+    @PutMapping("/")
+    public Todo updateTodo(@RequestBody Todo todo) {
+        return todoService.addOrUpdate(todo);
+    }
+
+    @DeleteMapping("/{id}")
+    @ResponseStatus(HttpStatus.OK)
+    public void deleteTodo(@PathVariable long id) {
+        todoService.destroyTodo(id);
+    }
+}
