a416db13de5070d698c0c9a70c75708c1e7be4bc
sean-rw <seanrwhitley@gmail.com>
2023-01-06 23:16:21 -0500

    Adding more JPA annotations to Todo model

--- src/main/java/com/seanrw/todobackend/models/Todo.java
+++ src/main/java/com/seanrw/todobackend/models/Todo.java
@@ -1,17 +1,22 @@
 package com.seanrw.todobackend.models;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 
 @Entity
+@Table(name="todos")
 public class Todo {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
+    @Column(nullable = false)
     private String text;
+    @Column(nullable = false)
     private String complete;
 
     public Todo(String text, String isComplete) {
