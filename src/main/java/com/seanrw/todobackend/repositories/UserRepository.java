bc0c1c746f8b9f655a0f1b0a6dc23972661027aa
sean-rw <seanrwhitley@gmail.com>
2023-01-08 01:15:09 -0500

    Add custom query for login

--- src/main/java/com/seanrw/todobackend/repositories/UserRepository.java
+++ src/main/java/com/seanrw/todobackend/repositories/UserRepository.java
@@ -1,9 +1,13 @@
 package com.seanrw.todobackend.repositories;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import com.seanrw.todobackend.models.User;
 
 public interface UserRepository extends JpaRepository<User, Long> {
-    
+    @Query(value = "SELECT * FROM users WHERE username = ?1 AND password = ?2", nativeQuery = true)
+    User findByUsernameAndPassword(String username, String password);
 }
